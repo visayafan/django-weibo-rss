709933f35685de41217c7e60a92c82b41a3302b4
visayafan <visayafan@163.com>
2018-08-20 12:15:03 +0800

    默认表情图标转文字&&标题不长直接显示后面不跟省略号

--- weibo/views.py
+++ weibo/views.py
@@ -31,35 +31,45 @@ def get_full_text(status):
     return status['text']
 
 
-def format_status(status):
-    text = get_full_text(status)
+def format_status(request, status):
+    description = get_full_text(status)
     # 该条微博有转发别人微博
     if 'retweeted_status' in status:
-        text += '<br/><br/>'
+        description += '<br/><br/>'
         retweeted_user = status['retweeted_status']['user']
-        text += '<div style="border-left: 3px solid gray; padding-left: 1em;">转发<a href="{url}">@{name}</a>：{retweet}</div>'.format(
+        description += '<div style="border-left: 3px solid gray; padding-left: 1em;">转发<a href="{url}">@{name}</a>：{retweet}</div>'.format(
             url=retweeted_user['profile_url'],
-            name=retweeted_user['screen_name'], retweet=format_status(status['retweeted_status']))
+            name=retweeted_user['screen_name'], retweet=format_status(request, status['retweeted_status']))
+    # 处理表情图标，默认表情图标全部转成文字
+    b = BeautifulSoup(description, 'html.parser')
+    if not request.GET.get('emoji'):
+        icons = b.find_all('span', class_='url-icon')
+        # 去掉位置/链接前的图标，太大太难看
+        if icons is not None:
+            for icon in icons:
+                # 表情图标img标签的alt值类似"[哈哈]"
+                if icon.img.has_attr('alt') and ('[' in icon.img.get('alt')):
+                    icon.replace_with(icon.img.get('alt'))
+                # 不是表情图标直接删除，例如文章链接、地理位置前的图标
+                else:
+                    icon.decompose()
+    description = str(b)
     # 后跟所有图片
     if 'pics' in status:
         for pic in status['pics']:
-            text += '<br/><br/><a href="{large_url}" target="_blank"><img src="{small_url}"/></a>'.format(
+            # 默认显示小图，点击可打开大图
+            description += '<br/><br/><a href="{large_url}" target="_blank"><img src="{small_url}"/></a>'.format(
                 large_url=pic['large']['url'], small_url=pic['url'])
-    b = BeautifulSoup(text, 'html.parser')
-    icons = b.find_all('span', class_='url-icon')
-    # 去掉位置前的图标，太大太难看
-    if icons is not None:
-        for icon in icons:
-            if 'location' in icon.img['src']:
-                icon.decompose()
-    return str(b)
+    return description
 
 
 def format_title(description):
     b = BeautifulSoup(description, 'html.parser')
-    # 若第1句少于TITLE_MAX_LENGTH个字符则取第1句话作为标题
-    title = re.split(',|\.|\!|\?|，|。|！|？', b.get_text())[0][:TITLE_MAX_LENGTH] + '...'
-    return title
+    # 若微博内容文字少则直接做为标题
+    if len(b.text) <= TITLE_MAX_LENGTH:
+        return b.text
+    # 否则取第1句的前TITLE_MAX_LENGTH个字符作为标题
+    return re.split(r'[,.!?:;，。！？：；]', b.text)[0][:TITLE_MAX_LENGTH] + '...'
 
 
 def index(request, uid):
@@ -78,7 +88,7 @@ def index(request, uid):
         if 'mblog' in card:
             item = FeedItem()
             status = card['mblog']
-            item.description = format_status(status)
+            item.description = format_status(request, status)
             item.link = 'https://m.weibo.cn/status/{id}'.format(id=status['id'])
             item.title = format_title(item.description)
             items.append(item)
