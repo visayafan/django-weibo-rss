f59ef20fdb84d4775f56329fb336b8c861d6846b
visayafan <visayafan@163.com>
2018-08-26 23:20:56 +0800

    部署到heroku上，修改staticfile配置，添加必要依赖

--- weiborss/settings.py
+++ weiborss/settings.py
@@ -115,3 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
